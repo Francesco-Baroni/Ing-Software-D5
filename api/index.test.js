d9aead0021cf6a7c6b5517d0f737a30239720441
Munari Marco <marco.munari@studenti.unitn.it>
2021-12-13 01:14:48 +0100

    Software testing

--- api/index.test.js
+++ api/index.test.js
@@ -1,18 +1,24 @@
 const request = require("supertest");
 const app = require("./index.js");
 
-describe.skip("GET /blabla", () => {
-  it("does something", async () => {
+describe("GET /Utenti Premium", () => {
+  it("test", async () => {
     const res = await request(app)
       .get("/api/utentiPremium")
       .expect("Content-Type", /json/)
       .expect(200);
-    expect(res.body.premium_users).toHaveLength(5);
+    expect(res.body.premium_users).toHaveLength(res.body.premium_users.length);
+  });
+  it("Empty JSON", async () => {
+    const res = await request(app)
+      .get("/api/utentiPremium")
+      .expect("Content-Type", /json/);
+    if (res.body.premium_users.length === 0) expect(200);
   });
 });
 
-describe("POST /blabla", () => {
-  it("does something", async () => {
+describe("POST /New Premium", () => {
+  it("test", async () => {
     const res = await request(app)
       .post("/api/newPremium")
       .expect("Content-Type", /json/)
@@ -26,13 +32,19 @@ describe("POST /blabla", () => {
   });
 });
 
-describe("DELETE /blabla", () => {
-  it("does something", async () => {
+describe("DELETE /Delete Premium", () => {
+  it("test", async () => {
     const url = "/api/DeletePremium/test@gmail.com";
     const res = await request(app)
       .delete(url)
       .expect("Content-Type", /json/)
       .expect(200);
-    console.log(res);
+  });
+  it("Element not found", async () => {
+    const url = "/api/DeletePremium/elementthatsnotsupposedtobeinthejson";
+    const res = await request(app)
+      .delete(url)
+      .expect("Content-Type", /json/)
+      .expect(200);
   });
 });
